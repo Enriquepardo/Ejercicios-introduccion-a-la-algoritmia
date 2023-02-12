5595e95ba6d0e9a7be4f5b2a5b37f6083ef3c53d
Enriquepardo <epardser@myuax.com>
2023-02-12 13:07:20 +0100

    ejercicio 8

---
+++ Ejercicio 8/Intereses_generados.py
@@ -0,0 +1,19 @@
+import functools
+
+def deco_intereses_generados(func):
+  def wrapper():
+    capital = float(input("Introduce el capital: "))
+    tasa_interes = float(input("Introduce la tasa de interés: "))
+    tiempo_en_meses = float(input("Introduce el tiempo en meses: "))
+
+    intereses = capital * tasa_interes * tiempo_en_meses / 100 / 12
+
+    print("El importe de los intereses generados es:", intereses)
+
+  return wrapper
+
+@deco_intereses_generados
+def intereses_generados():
+  pass
+
+intereses_generados()
---
+++ Ejercicio 8/Precio_impuestos.py
@@ -0,0 +1,20 @@
+import functools
+
+def deco_precio_impuestos_incluidos(func):
+    def wrapper():
+        precio_sin_impuestos = float(input('Introduce el precio sin impuestos: '))
+        porcentaje_IVA = float(input('Introduce el porcentaje de IVA: '))
+        TII = precio_sin_impuestos + (precio_sin_impuestos * porcentaje_IVA / 100)
+
+        print('El precio con impuestos incluidos es: ', TII)
+
+    return wrapper
+
+
+@deco_precio_impuestos_incluidos
+def precio_con_impuestos_incluidos():
+  pass
+
+precio_con_impuestos_incluidos()
+
+
